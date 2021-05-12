e6121390329929ae492861779dd2242aa40275df
minji <zndnfjqbd1206@gmail.com>
2021-05-12 14:44:08 +0900

    20210512

--- main.cpp
+++ main.cpp
@@ -2,23 +2,24 @@
 #include <iostream>
 #include "point.hpp"
 void on_mouse(int event, int x, int y, int flags, void* param);
-
+click c;
 
 int main()
 {
 	cv::Mat img = cv::imread("tory.jpg", cv::IMREAD_COLOR);
-
+	//cv::Mat out = img.clone();
 	if (img.empty())
 	{
 		std::cout << "Image load failed";
 		return -1;	
 	}
 	cv::namedWindow("img");
-	click c;
+	//cv::namedWindow("out");
 
 	c.count = 0;
 	cv::setMouseCallback("img", on_mouse, &img);
 	cv::imshow("img", img);
+	//cv::imshow("out", out);
 	cv::waitKey();
 
 	return 0;
@@ -42,33 +43,5 @@ void on_mouse(int event, int x, int y, int flags, void*param)
 		std::cout << c.count << std::endl;
 		c.line(&img, point);
 	}
-	//if (c.count % 4 == 1)
-	//{
-	//	c.p1.x = point.x;
-	//	c.p1.y = point.y;
-	//	std::cout << c.p1.x << " p1 " << c.p1.y << std::endl;
-	//}
-	//if (c.count % 4 == 2)
-	//{
-	//	c.p2.x = point.x;
-	//	c.p2.y = point.y;
-	//	std::cout << c.p1.x << " p1 " << c.p1.y << std::endl;
-	//	std::cout << c.p2.x << " p2 " << c.p2.y << std::endl;
-	//	cv::line(img, c.p1, c.p2, cv::Scalar(0, 255, 0), 3);
-	//	//cv::imshow("img", img);
-	//}
-	//if (c.count % 4 == 3)
-	//{
-	//	c.p3.x = point.x;
-	//	c.p3.y = point.y;
-	//	std::cout << c.p3.x << " p3 " << c.p3.y << std::endl;
-	//}
-	//
-	//if (c.count % 4 == 0)
-	//{
-	//	c.p4.x = point.x;
-	//	c.p4.y = point.y;
-	//	std::cout << c.p4.x << " p4 " << c.p4.y << std::endl;
-	//}
-	//cv::imshow("img", img);
+	
 }
--- point.hpp
+++ point.hpp
@@ -17,6 +17,12 @@ class click {
 		{
 			cv::Mat img;
 			img = *(cv::Mat*)param;
+			std::vector<cv::Point> trans;
+			cv::Point2f src[4], dst[4];
+			cv::Mat apple = cv::imread("apple.jpg", cv::IMREAD_COLOR);
+			cv::resize(apple, apple, cv::Size(img.cols, img.rows));
+			cv::Mat destination(1000, 1000, CV_8UC3);
+
 			if (count % 4 == 1)
 			{
 				p1.x = p.x;
@@ -29,7 +35,7 @@ class click {
 				p2.y = p.y;
 				std::cout << p1.x << " p1 " << p1.y << std::endl;
 				std::cout << p2.x << " p2 " << p2.y << std::endl;
-				cv::line(img, p1, p2, cv::Scalar(0, 255, 0), 3);
+				
 				//cv::imshow("img", img);
 			}
 			if (count % 4 == 3)
@@ -44,7 +50,43 @@ class click {
 				p4.x = p.x;
 				p4.y = p.y;
 				std::cout << p4.x << " p4 " << p4.y << std::endl;
+				cv::line(img, p1, p2, cv::Scalar(0, 255, 0), 3);
+				cv::line(img, p2, p3, cv::Scalar(0, 255, 0), 3);
+				cv::line(img, p3, p4, cv::Scalar(0, 255, 0), 3);
+				cv::line(img, p4, p1, cv::Scalar(0, 255, 0), 3);
+				/*trans.push_back(p1);
+				trans.push_back(p2);
+				trans.push_back(p3);
+				trans.push_back(p4);*/
+
+				src[0] = cv::Point2i(p1.x, p1.y);
+				src[1] = cv::Point2i(p2.x, p2.y);
+				src[2] = cv::Point2i(p3.x, p3.y);
+				src[3] = cv::Point2i(p4.x, p4.y);
+				
+				int max_w = (p4.x - p1.x > p3.x - p2.x) ? p4.x - p1.x : p3.x - p2.x;
+				int max_h = (p2.y - p1.y > p3.y - p4.y) ? p2.y - p1.y : p3.y - p4.y;
+
+				dst[0] = cv::Point2i(0, 0);
+				dst[1] = cv::Point2i(0, max_h-1);
+				dst[2] = cv::Point2i(max_w-1, max_h-1);
+				dst[3] = cv::Point2i(max_w-1, 0);
+
+				cv::Point2f temp_src[4], temp_dst[4];
+				temp_src[0] = cv::Point2f(0, 0);
+				temp_src[1] = cv::Point2f(0, img.rows);
+				temp_src[2] = cv::Point2f(img.cols, img.rows);
+				temp_src[3] = cv::Point2f(img.cols, 0);
+
+				cv::Mat result = cv::getPerspectiveTransform(temp_src, src);
+				cv::warpPerspective(img, destination, result, cv::Size(img.cols, img.rows));
+
 			}
+			
 			cv::imshow("img", img);
+			cv::imshow("apple", apple);
+			cv::imshow("out", destination);
 		}
+
+		//void transform();
 };
